fd736c67796dbcffd6650e439c5cb3d0c380fd0c
speedyGonzales <yonchoy@abv.bg>
2015-01-19 15:10:47 +0200

    ^X first commit

---
+++ solutions/01.rb
@@ -0,0 +1,25 @@
+def fibonacci(number)
+    if [1,2].include?(number)
+        return 1
+    else
+        return fibonacci(number-1) + fibonacci(number-2)
+    end
+end
+
+def lucas(number)
+    case number
+    when 1 then return 2
+    when 2 then return 1
+    else
+        return lucas(number-1)+lucas(number-2)
+    end
+end
+
+def series(name, number)
+    case name
+    when 'fibonacci' then return fibonacci(number)
+    when 'lucas' then return lucas(number)
+    else
+        return fibonacci(number)+lucas(number)
+    end
+end
